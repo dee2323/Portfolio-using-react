153ba6ecce6f1f05365d909cd9e0f4518936bf21
dee2323 <d.k.isleem@students.ptuk.edu.ps>
2022-12-16 22:29:16 +0200

    footer section

---
+++ src/components/Footer/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import './style.css'
+const Footer = () => {
+    return (<footer>
+        <ul class="socialIcons">
+            <li><a href="https://github.com/dee2323" target="_blank"><i class="fa-brands fa-github"></i></a></li>
+            <li><a href="https://www.linkedin.com/in/deemaisleem" target="_blank"><i
+                class="fa-brands fa-linkedin-in"></i></a></li>
+            <li><a target="_blank" href="mailto:deemasleem1@"><i class="fa-brands fa-google"></i></a></li>
+            <li><a target="_blank"
+                href="https://www.facebook.com/people/%D8%AF%D9%8A%D9%85%D8%A9-%D8%B3%D9%84%D9%8A%D9%85/100018442142873/"><i
+                    class="fa-brands fa-facebook"></i></a></li>
+        </ul>
+        <p>Designed & Developed by Deema Saleem &#169; 2022</p>
+
+    </footer>)
+}
+export default Footer;
